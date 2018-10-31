b9e1a75e83f99eeddaa6e15dfabbf7db3314ef7a
Yanish Ramboucs <yanishrambocus@gmail.com>
2018-10-31 10:07:34 -0400

    Changes

--- LinkedList.h
+++ LinkedList.h
@@ -19,11 +19,69 @@ class LinkedList
      void add(T element);
 
      T get(int index) const;  // note: a const method
-     T remove(int index); 
+     T remove(int index);
 
      std::vector<T> toArray() const;  // note: a const method
 
      LinkedList<T>& operator+=( const T& item );
 };
+
+template <class T>
+LinkedList::LinkedList() {}
+
+template <class T>
+LinkedList::LinkedList(const LinkedList<T>& other) {}
+
+template <class T>
+LinkedList::~LinkedList() {}
+
+template <class T>
+void LinkedList::add(T element)
+{
+  return;
+}
+
+template <class T>
+int LinkedList::size()
+{
+  return 0;
+}
+
+template <class T>
+T LinkedList::get(int index)
+{
+  //if ( index < 0 || theList.size() < index )
+
+  list<T>::const_iterator it = theList.begin();
+
+  while (it != theList.begin() + index )
+  {
+    it++;
+  }
+  return *it;
+
+}
+
+template <class T>
+T LinkedList::remove(int index)
+{
+  list<T>::const_iterator it = theList.begin();
+
+  while (it != theList.begin() + index )
+  {
+    it++;
+  }
+  return *it;
+}
+template <class T>
+vector<T> LinkedList::toArray()
+{
+  vector<T> returnV();
+  return returnV;
+}
+
+template <class T>
+LinkedList <T>& LinkedList::operator+=(const T& item)
+
 
 #endif
--- tester.cpp
+++ tester.cpp
@@ -6,15 +6,24 @@
 
 int main ()
 {
-    std::cout << "These are the tests: " << std::endl;
+    std::cout << "These tests will help the development of the LinkedList class: " << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+
+    std::cout << "LinkedList<int> intList()" << std::endl;
 
     LinkedList<int> intList();
     intList.add(10);
     intList.add(20);
 
     stdt::cout << "Size: " <<intList.size(); << std::endl;
+    std::cout << "\tExpected : [2]" << std::endl;
 
-    std::cout << << intList.get(0) << std::endl;
-    std::cout << "Expected: [10]" << std::endl;
+    std::cout << "intList.get(0): " << intList.get(0) << std::endl;
+    std::cout << "\tExpected: [10]" << std::endl;
+
+    intList.remove(0);
+
+    std::cout << "Size:" << intList.size() << std::endl;
+    std::cout << "\tExpected: [1]" << std::endl;
 
 }
